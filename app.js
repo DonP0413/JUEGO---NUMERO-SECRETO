9485553535ea1eedbefc392bb05a9cf8b8bfb351
Miguel <akmp2019@outlook.com>
2025-08-13 13:28:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+let numeroSecreto=0;
+let intentos=1;
+let listaNumerosSorteados= [];
+let numeroMaximo =10;
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML=document.querySelector(elemento);
+    elementoHTML.innerHTML= texto;
+    return;
+}
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value) ;
+    if(numeroDeUsuario===numeroSecreto ){
+        asignarTextoElemento("p","acertaste el número en :" +intentos + "intentos");
+       document.getElementById("reiniciar").removeAttribute("disabled");
+    }else {
+        // el usuario no acerto
+        if (numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento("p","El numero secreto es menor");
+        } else {
+            asignarTextoElemento("p","El numero secreto es mayor");
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+    }
+    function limpiarCaja (){
+       document.querySelector("#valorUsuario").value ="";
+        
+    }
+    function generarNumeroSecreto() {
+    let numeroGenerado=  Math.floor(Math.random()*numeroMaximo)+1;
+    // si ya sorteamos todos los números.
+    if (listaNumerosSorteados.length==numeroMaximo){
+        asignarTextoElemento("p","Ya se sortearon todos los números posibles");
+    }else{
+     // si el numero generado esta incluido en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)){
+        return generarNumeroSecreto();
+    } else {
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+            
+    }   
+    }
+    };
+    function condicionesIniciales(){
+        asignarTextoElemento("h1","Juego del número secreto");
+        asignarTextoElemento("p","Ingresa un número del 1 al "+numeroMaximo);
+        numeroSecreto= generarNumeroSecreto();
+        intentos=1;
+    }
+    function reiniciarJuego() {
+        // limpiar la caja
+        limpiarCaja();
+        // indicar mensaje de inicio
+        // generar el nuevo número aleatorio        
+        // inicializar numero de intentos.
+        condicionesIniciales();
+        // deshabilitar boton de nuevo juego
+        document.getElementById("reiniciar").setAttribute("disabled","true");
+        
+    }
+    
+    condicionesIniciales();
